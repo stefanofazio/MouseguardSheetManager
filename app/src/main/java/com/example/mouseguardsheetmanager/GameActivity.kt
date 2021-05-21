c69152a70976c557d85ca13fe2b266fd354e9771
stefano.fazio <stefanofazio@outlook.it>
2021-05-21 15:29:25 +0200

    Introduzione delle principali activity che compongono l'applicazione. Definite le due customrows necessarie per le listview.

--- app/src/main/java/com/example/mouseguardsheetmanager/GameActivity.kt
+++ app/src/main/java/com/example/mouseguardsheetmanager/GameActivity.kt
@@ -1,11 +1,66 @@
 package com.example.mouseguardsheetmanager
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.Button
+import android.widget.ListView
+import android.widget.TextView
 
 class GameActivity : AppCompatActivity() {
+    private lateinit var gamesList:ListView
+    private lateinit var gamesButton: Button
+    private var role: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
+        gamesList = findViewById(R.id.gamesListView)
+        gamesButton = findViewById(R.id.gamesButton)
+        role = intent.getStringExtra("role").toString()
+        setButtonText()
+        setListViewContent()
+    }
+
+    private fun setButtonText()
+    {
+        if (role == "master") { gamesButton.text = "Crea" }
+        else { gamesButton.text = "Unisciti" }
+    }
+
+    private fun setListViewContent()
+    {
+        var myAdapter: GameAdapter = GameAdapter(this, 18)
+        gamesList.adapter = myAdapter
+    }
+}
+
+class GameAdapter(private val context: Context, private val elements : Int) : BaseAdapter()
+{
+    override fun getCount(): Int {
+        return elements
+    }
+
+    override fun getItem(position: Int): Any {
+        return position
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+        val rowView = LayoutInflater.from(context).inflate(R.layout.game_row, parent, false)
+        var gameName: TextView = rowView.findViewById(R.id.gameNameText)
+        var masterName: TextView = rowView.findViewById(R.id.masterNameText)
+        var playerNumber: TextView = rowView.findViewById(R.id.playerNumberText)
+        gameName.text = "Partita numero " + position.toString()
+        masterName.text = "Master numero " + position.toString()
+        playerNumber.text = "Numero giocatori: " + (1..15).random().toString()
+        return rowView
     }
 }
--- app/src/main/java/com/example/mouseguardsheetmanager/ModeActivity.kt
+++ app/src/main/java/com/example/mouseguardsheetmanager/ModeActivity.kt
@@ -3,15 +3,31 @@ package com.example.mouseguardsheetmanager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.*
+import android.content.Intent
 
 class ModeActivity : AppCompatActivity() {
+
+    private var role: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_mode)
+        role = intent.getStringExtra("role").toString()
     }
 
     fun getClick(view: View?)
     {
-
+        val gameIntent: Intent
+        if (view?.id == R.id.gameButton)
+        {
+            gameIntent = Intent(this, GameActivity::class.java)
+            gameIntent.putExtra("role",role)
+        }
+        else
+        {
+            gameIntent = Intent(this, CharactersActivity::class.java)
+            gameIntent.putExtra("role", role)
+        }
+        startActivity(gameIntent)
     }
 }
